f9987f302d9354f6eab1891bd1c876343352f5df
Hailey Foster <haileyhasmail@gmail.com>
2016-11-19 18:12:51 -0800

    Build dice with SVG

--- intro-to-elm/dice.elm
+++ intro-to-elm/dice.elm
@@ -2,10 +2,16 @@ module Main exposing (..)
 
 import Html exposing (..)
 import Html.Events exposing (onClick)
-import Html.Attributes exposing (..)
+
+
+-- import Html.Attributes exposing (..)
+
 import Html.App as App
 import Random exposing (..)
 import String
+import Svg exposing (..)
+import Svg.Attributes exposing (..)
+import Array
 
 
 main : Program Never
@@ -21,11 +27,6 @@ type alias Dice =
     { diceFace1 : Int
     , diceFace2 : Int
     }
-
-
-faces : List String
-faces =
-    [ "http://www.clipartkid.com/images/170/dice-faces-clipart-1-9-reaching-teachers-labd0b-clipart.png" ]
 
 
 init : ( Dice, Cmd Msg )
@@ -69,15 +70,109 @@ diePairGenerator =
 view : Dice -> Html Msg
 view model =
     div []
-        [ img [ src (getImg model.diceFace1) ] []
-        , button [ onClick Roll ] [ text "Roll" ]
-        , img [ src (getImg model.diceFace2) ] []
+        [ button [ onClick Roll ] [ Html.text "Roll" ]
+        , div []
+            [ makeFace model.diceFace1
+            , makeFace model.diceFace2
+            ]
         ]
 
 
 getImg : Int -> String
 getImg diceFace =
     String.concat [ "./dice/", toString diceFace, ".jpg" ]
+
+
+makeFace : Int -> Html Msg
+makeFace diceFace =
+    let
+        faces =
+            Array.fromList [ makeOne, makeTwo, makeThree, makeFour, makeFive, makeSix ]
+
+        face =
+            Array.get diceFace faces
+    in
+        Svg.svg
+            [ Svg.Attributes.width "120"
+            , Svg.Attributes.height "120"
+            , viewBox "0 0 120 120"
+            ]
+            [ g []
+                [ rect
+                    [ x "10"
+                    , y "10"
+                    , Svg.Attributes.width "100"
+                    , Svg.Attributes.height "100"
+                    , rx "15"
+                    , ry "15"
+                    ]
+                    []
+                , Maybe.withDefault makeOne face
+                ]
+            ]
+
+
+makeOne : Svg a
+makeOne =
+    g []
+        [ diceCircle "60" "60" ]
+
+
+makeTwo : Svg a
+makeTwo =
+    g []
+        [ diceCircle "35" "35"
+        , diceCircle "85" "85"
+        ]
+
+
+makeThree : Svg a
+makeThree =
+    g []
+        [ diceCircle "35" "35"
+        , diceCircle "60" "60"
+        , diceCircle "85" "85"
+        ]
+
+
+makeFour : Svg a
+makeFour =
+    g []
+        [ diceCircle "35" "35"
+        , diceCircle "35" "85"
+        , diceCircle "85" "35"
+        , diceCircle "85" "85"
+        ]
+
+
+makeFive : Svg a
+makeFive =
+    g []
+        [ diceCircle "35" "35"
+        , diceCircle "35" "85"
+        , diceCircle "85" "35"
+        , diceCircle "60" "60"
+        , diceCircle "85" "85"
+        ]
+
+
+makeSix : Svg a
+makeSix =
+    g []
+        [ diceCircle "35" "35"
+        , diceCircle "35" "60"
+        , diceCircle "35" "85"
+        , diceCircle "85" "35"
+        , diceCircle "85" "60"
+        , diceCircle "85" "85"
+        ]
+
+
+diceCircle : String -> String -> Svg a
+diceCircle x y =
+    circle
+        [ cx x, cy y, r "10", fill "white" ]
+        []
 
 
 
